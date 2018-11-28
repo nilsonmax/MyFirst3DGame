12f317c7b719da727453786b44bd732ad9bb4231
nilsonmax <nilsonbenitez@hotmail.com>
2018-11-28 10:33:36 -0500

    Initial commit

---
+++ Assets/Coin/RecogedorDeObjeto.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RecogedorDeObjeto : MonoBehaviour
+{
+    public static int coinsCounts=0;
+	// Use this for initialization
+	void Start ()
+    {
+        coinsCounts++;
+        Debug.Log("Hay"+ coinsCounts + "monedas");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            coinsCounts--;
+            if (coinsCounts==0)
+            {
+                Debug.Log("Has Ganado");
+                GameObject GameManager = GameObject.Find("GameManager");
+                Destroy(GameManager);
+                GameObject[] FireWorksSystem = GameObject.FindGameObjectsWithTag("Fireworks");
+
+                foreach (GameObject Fireworks in FireWorksSystem)
+                {
+                    Fireworks.GetComponent<ParticleSystem>().Play();
+                }
+            }
+            Destroy(gameObject);
+        }
+        
+    }
+}
---
+++ Assets/Coin/Timer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Timer : MonoBehaviour {
+
+    // Use this for initialization
+    public float maxTime = 60f;
+    private float countDown = 0f;
+
+    void Start () {
+        countDown = maxTime;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        countDown -= Time.deltaTime;
+        Debug.Log("cuenta atras :" + countDown);
+        if (countDown<=0)
+        {
+            Debug.Log("Has perdido :");
+            RecogedorDeObjeto.coinsCounts = 0;
+            SceneManager.LoadScene("MainScene");
+        }
+	}
+}
